5937e151db67bce4fd908112935592b98f12df63
wontae <beegramin9@gmail.com>
2021-07-12 17:42:02 +0900

    finish refactoring calculator

--- 계산기/app.js
+++ 계산기/app.js
@@ -1,79 +1,89 @@
-let formula = "";
-
 const buttons = document.querySelectorAll('.button')
-const stopButton = document.querySelector('.button-stop')
-const resetButton = document.querySelector('.button-reset')
+const displayElement = document.querySelector('input')
 
-/* 이벤트핸들러로 하자 */
+/* 예외처리
+1. 2++3 처럼 연산자를 연속으로 입력할 수 있다.
+2. +3 처럼 숫자보다 연산자를 먼저 입력할 수 있다.
+3. 6* 처럼 식이 완전하게 끝나지 않은 사태에서 = 버튼을 클릭하면 에러가 발생한다.
 
-const operators = ['+','-','*','/']
+4. 2+3 = 5로 계산이 끝난 뒤 새로운 계산을 하기 위해 3을 입력하면 53으로 표시된다.
+= 버튼을 클릭 후 숫자를 클릭한다면 AC기능과 비슷하게 새로운 식 입력을 시작하는거니
+appendNumber 메서드에서 this.displayContent += number로 숫자가 추가되던걸
+= 버튼을 클릭 후 처음으로 숫자를 클릭할 때만 this.displayContent = number로 바로 초기화되게 한다.
+= 버튼을 클릭했는지 알아내기 위해 operatorCheck처럼 equalsCheck 속성을 하나 추가해 = 버튼 클릭 여부를 관리한다 */
 
+class Calculator {
+    constructor(displayElement) {
+        this.displayElement = displayElement
+        this.displayContent = '' // 계산되고있는 숫자들
+        // 2번, 처음부터 연산자가 입력되는 걸 막기 위해 기본값을 true로 변경
+        this.operatorCheck = true 
+        this.equalsCheck = false // 4번, 버튼클릭여부
+    }
 
-/* 곱하기, 나누기할때만 2승들이 계쏙 되어야되고
-+,-일때는 =을 해야먄 보여줘야 함 */
-// function handleClick(event) {
-//     const num = event.target.value;
-//     if (num === '=') {
-//         if (formula){
-//             document.querySelector('#result').innerText = eval(formula);
-//         } else {
-//             /* 아무것도 입력하지 않았는데 = 누르면 그대로 0 */ 
-//             document.querySelector('#result').innerText = "0";
-//         }
-//     } else if (num === 'C') {
-//         formula = "";
-//         document.querySelector('#result').innerText = "0";
-//     } else if (num === "*" || num === '/') {
-//         /* 할때마다 올라와야 하는데...
-//         기호를 누르면 되긴 되는데.? */
-// 
-//         /* 아 그냥 기호만 할까... */
-//         formula += num;
-//         document.querySelector('#result').innerText = eval(formula);
-//         /* 여기에 바로 올려서 승수가 가능한 것임
-//         +, - 일땐 바로 안올리면 됨
-//         
-//         반대로*/
-//         /* console.log(formula) */
-//         console.log('더하기빼기')
-//     } else {
-//         formula += num;
-//     }
-// }
+    appendNumber(number) {
+        if (this.equalsCheck) {
+            this.displayContent = number // 새로운 식 입력, 초기화
+            this.equalsCheck = false
+        } else {
+            this.displayContent += number // 문자열 concatnation
+
+        }
+        this.operatorCheck = false // 1번, 숫자 입력 시 다음에 연산자 올 수 있게 false
+    }
 
 
-function handleClick(event) {
-    const num = event.target.value;
-    formula += num
-    if (operators.includes(num)) {
+    appendOperator(operator) {
+        if (this.operatorCheck) return // 1번, 이미 연산자가 입력된 상태
+        if (this.equalsCheck) this.equalsCheck = false // 4번
+        this.displayContent += operator
+        return this.operatorCheck = true
+    }
 
-        /* 중요한 것은 보여주는데애는 +-가 들어가면 안되지만...? */
-        console.log(1);
-    } else {
-        /* 연산자 눌렀을 때는 안됨 */
-        document.querySelector('#result').innerText = formula
-        console.log(2);
-    } 
-    
+    updateDisplay() {
+        this.displayElement.value = this.displayContent
+    }
+
+    reset() {
+        this.displayContent = ''
+        this.displayElement.value = 0
+        /* reset 이후에 연산자가 입력될 수 없도록 막는다 */
+        this.operatorCheck = true
+    }
+
+    compute() {
+        // 3번, 연산자가 마지막으로 입력되었을 때
+        if (this.operatorCheck) return 
+        this.displayContent = eval(this.displayContent)
+        this.equalsCheck = true
+    }
 }
+const calculator = new Calculator(displayElement)
 
-function handleStopButton() {
-    const result = eval(formula)
-    /* 앞에거는 해결이 되어야 함 */
-    console.log(formula)
-    document.querySelector('#result').innerText = result;
-}
+buttons.forEach(button => {
+    button.addEventListener('click', () => {
+        switch (button.dataset.type) {
+            case 'operator':
+                console.log('operator');
+                if ( calculator.appendOperator(button.innerText) ) {
+                    calculator.updateDisplay()
+                }
+                break
+            case 'reset':
+                console.log('reset');
+                calculator.reset();
+                break
+            case 'equal':
+                console.log('equal');
+                calculator.compute();
+                calculator.updateDisplay();
+                break
+            default:
+                console.log('number');
+                calculator.appendNumber(button.innerText)
+                calculator.updateDisplay()
+                break
+        }
+    }
+)})
 
-function handleResetButton() {
-    formula = ""
-    document.querySelector('#result').innerText = "0"
-}
-
-function init() {
-    /*  querySelectorAll은 Array비슷한 NodeList를 주는데 
-    이것은 forEach메서드를 사용할 수 있습니다.*/
-    buttons.forEach(button => button.addEventListener('click',handleClick));
-    stopButton.addEventListener('click',handleStopButton);
-    resetButton.addEventListener('click',handleResetButton);
-}
-init()
